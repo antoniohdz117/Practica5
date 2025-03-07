e8848a8f27b2ceeb65cfe223593af34af62422c8
antoniohdz117 <antonio117.berrios@gmail.com>
2025-03-06 21:16:37 -0600

    practica 5 parte 2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,45 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Navbar from './Navbar';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import {Container, Link, Typography} from "@mui/material";
 const theme = createTheme();
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Navbar />
+            <CssBaseline/>
+            <Navbar/>
             <h1>Proyecto 5</h1>
-            <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-                <TextField variant="outlined" placeholder="No se" />
-            </Box>
-        </ThemeProvider>
+            </ThemeProvider>
     );
+
+
+}
+function Copyright() {
+    return (
+        <Typography sx={{mb: 2}}>
+            { 'Copyright ' }
+            <Link href="https://github.com/antoniohdz117">
+                {' Git '}
+            </Link>
+            { new Date().getFullYear() }
+        </Typography>);
 }
 
-export default App;
+
+
+
+//export default App;
+export default function MyApp() {
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{my: 4}}>
+                <App/>
+                <Typography variant="h4" component="h1" sx={{mb: 2}}>
+                    Mi primera App con React y Material UI
+                </Typography>
+                <Copyright/>
+            </Box>
+        </Container>
+    );
+}
